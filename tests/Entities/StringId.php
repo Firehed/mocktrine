df9034902f2d1b31bfad8724f9f6cf5a947a2494
Eric Stern <eric@slant.co>
2019-12-20 14:42:00 -0800

    red: test string ids

---
+++ tests/Entities/StringId.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Firehed\Mocktrine\Entities;
+
+/**
+ * @Entity
+ * @Table(name="string_ids")
+ */
+class StringId
+{
+    /**
+     * @Id
+     * @Column(type="string")
+     * @GeneratedValue
+     * @var ?string
+     */
+    private $id;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+}
