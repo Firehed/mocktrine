7e4bc53792f3e617c1e4f172af349bfa6ffe93c1
Eric Stern <eric@ericstern.com>
2023-07-17 10:45:00 -0700

    Add rest of attributes

--- tests/Entities/StringId.php
+++ tests/Entities/StringId.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Firehed\Mocktrine\Entities;
 
+use Doctrine\ORM\Mapping;
+
 /**
  * @Entity
  * @Table(name="string_ids")
  */
+#[Mapping\Entity]
+#[Mapping\Table(name: 'string_ids')]
 class StringId
 {
     /**
@@ -16,6 +20,9 @@ class StringId
      * @GeneratedValue
      * @var ?string
      */
+    #[Mapping\Id]
+    #[Mapping\Column]
+    #[Mapping\GeneratedValue]
     private $id;
 
     public function getId(): ?string
--- tests/Entities/UnspecifiedId.php
+++ tests/Entities/UnspecifiedId.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Firehed\Mocktrine\Entities;
 
+use Doctrine\ORM\Mapping;
+
 /**
  * @Entity
  * @Table(name="unspecified_ids")
  */
+#[Mapping\Entity]
+#[Mapping\Table(name: 'unspecified_ids')]
 class UnspecifiedId
 {
     /**
@@ -16,6 +20,9 @@ class UnspecifiedId
      * @GeneratedValue
      * @var ?string
      */
+    #[Mapping\Id]
+    #[Mapping\Column]
+    #[Mapping\GeneratedValue]
     private $id;
 
     public function getId(): ?string
